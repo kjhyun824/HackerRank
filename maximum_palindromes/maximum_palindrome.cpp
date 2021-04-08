c0d99e539adb2db04883a1923e23796a9ab76798
Junghyun Kim <woori4829@gmail.com>
2021-04-09 00:48:49 +0900

    Maximum Palindrome - In progress (Factorial)

--- maximum_palindromes/maximum_palindrome.cpp
+++ maximum_palindromes/maximum_palindrome.cpp
@@ -2,12 +2,37 @@
 #include <string>
 using namespace std;
 
-void initialize(string s) {
+const int mod_prime = 1000000007;
+const int max_n = 100000;
+const int max_l = 26;
+int counts[max_n][max_l] = {0};
+int even_count[max_l] = {0};
+int facts[max_n];
+int invs[max_n];
 
+void initialize(string s) {
+  counts[0][s[0] - 'a'] = 1;
+  facts[0] = 1;
+  for (int i = 1; i < s.length(); i++) {
+    for (int j = 0; j < max_l; j++) {
+      counts[i][j] = counts[i - 1][j];
+    }
+    counts[i][s[i] - 'a']++;
+  }
 }
 
 int answerQuery(int l, int r) {
-  int output = 0;
+  int output = 1;
+  int num_odds = 0;
+  int num_evens = 0;
+
+  for (int i = 0; i < max_l; i++) {
+    int count = counts[r][i] - counts[l][i];
+    num_odds += count % 2;
+    even_count[i] = count / 2;
+    num_evens += even_count[i];
+  }
+
   return output;
 }
 
